58d568aa60051d8d2aea54d507b2a37cb9d85845
myrdinaptaliesen <nicog.dev@gmail.com>
2020-05-29 15:01:34 +0200

    Update session.php

--- Exemple-Session/session.php
+++ Exemple-Session/session.php
@@ -1,18 +1,21 @@
 <?php
-   session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
-   <head>
-       <title>Cours PHP & MySQL</title>
-       <meta charset="utf-8">
-       <link rel="stylesheet" href="cours.css">
-   </head>
-  
-   <body>
-       <?php
-           echo 'Bonjour ' .$_SESSION['prenom']. ',
-           tu as ' .$_SESSION['age']. ' ans';
-       ?>
-   </body>
+
+<head>
+    <title>Cours PHP & MySQL</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="cours.css">
+</head>
+
+<body>
+    <?php
+    echo 'Bonjour ' . $_SESSION['prenom'] . ',
+           tu as ' . $_SESSION['age'] . ' ans';
+
+    ?>
+</body>
+
 </html>
