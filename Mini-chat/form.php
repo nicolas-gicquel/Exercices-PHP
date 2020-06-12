59ae6b076e8bbe28279c8acb93f397a5ba8d1d6b
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-12 14:36:52 +0200

    Update form.php

--- Mini-chat/form.php
+++ Mini-chat/form.php
@@ -1,13 +1,13 @@
 <?php
 
-//On crée la base de données//
+//3-On crée la base de données//
 $pdo = new PDO('mysql:host=localhost;port=3308','root',''); 
 $sql = "CREATE DATABASE IF NOT EXISTS `Minichat` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci";
 $pdo->exec($sql);
 
-//Creation de la table users//
+//4-Creation de la table users//
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=ProjetX;port=3308','root','',
+    $pdo = new PDO('mysql:host=localhost;dbname=minichat;port=3308','root','',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
  
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Ligne qui permet d'afficher les erreurs s'il y en a.
@@ -15,7 +15,7 @@ try{
     $sql = "CREATE TABLE IF NOT EXISTS `minichat`.`users` ( 
         `id` INT NOT NULL AUTO_INCREMENT , 
         `pseudo` VARCHAR(50) NOT NULL , 
-        `message` VARCHAR(1000) NOT NULL ,  
+        `message` VARCHAR(255) NOT NULL ,  
         `dateMessage` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , 
         PRIMARY KEY (`id`)) ENGINE = InnoDB;";
  
@@ -28,7 +28,7 @@ try{
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
  }
-
+//5- On remplit la base de données grâce au formulaire
  try{
     $pdo = new PDO('mysql:host=localhost;dbname=minichat;port=3308','root','',
     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
@@ -37,19 +37,18 @@ try{
    
     $pseudo = $_POST['pseudo'];
     $message = $_POST['message'];
-    echo $pseudo;
-    echo $message;
+
 
  
    //$sth appartient à la classe PDOStatement
     $sth = $pdo->prepare("
         INSERT INTO Users(pseudo,message)
-        VALUES (:pseudo, :truc)
+        VALUES (:pseudo, :message)
     ");
     
     $sth->execute(array(
                         ':pseudo' => $pseudo,
-                        ':truc' => $message));
+                        ':message' => $message));
  
 }
      
@@ -57,7 +56,8 @@ catch(PDOException $e){
     echo "Erreur : " . $e->getMessage();
 }
 
+//6- On redirige vers la page index.php après l'exécution du script
 header('location:index.php');
 
-
+?>
 
