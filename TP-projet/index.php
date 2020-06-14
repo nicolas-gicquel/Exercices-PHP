fb02d3cd22b1e10469a8bd0c47889792e81601b6
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-14 23:01:18 +0200

    Update index.php

--- TP-projet/index.php
+++ TP-projet/index.php
@@ -16,8 +16,8 @@
         <div class="nav-wrapper">
             <a href="index.php" class="brand-logo">Logo</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="sass.html">Ma petite boutique</a></li>
-                <li><a href="badges.html">Connexion</a></li>
+                <li><a href="index.php">Ma petite boutique</a></li>
+                <li><a href="admin.php">Connexion</a></li>
             </ul>
         </div>
     </nav>
