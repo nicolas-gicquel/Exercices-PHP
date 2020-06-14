075e5f83b5da780eb2af586ebc6a58561c14f74e
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-14 23:01:15 +0200

    Update admin.php

--- TP-projet/admin.php
+++ TP-projet/admin.php
@@ -7,7 +7,9 @@
     <title>Ma petite boutique</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
 
 
 </head>
@@ -19,7 +21,7 @@
             <a href="index.php" class="brand-logo">Logo</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="index.php">Ma petite boutique</a></li>
-                <li><a href="badges.html">Déconnexion</a></li>
+                <li><a href="index.php">Déconnexion</a></li>
             </ul>
         </div>
     </nav>
