600fe0a19f082e5100d5f768c2c09d55a64e532c
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-15 16:47:43 +0200

    Create form.php

---
+++ TP-blog/form.php
@@ -0,0 +1,31 @@
+<?php
+try{
+    $pdo = new PDO('mysql:host=localhost;dbname=tpblog;port=3308','root','',
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+    //2- On récupère les données du formulaire
+    $titre = $_POST['titre'];
+    $contenu = $_POST['contenu'];
+
+
+ 
+   //$sth appartient à la classe PDOStatement
+    $sth = $pdo->prepare("
+        INSERT INTO posts(titre,contenu)
+        VALUES (:titre, :contenu)
+    ");
+    
+    $sth->execute(array(
+                        ':titre' => $titre,
+                        ':contenu' => $contenu));
+ 
+}
+     
+catch(PDOException $e){
+    echo "Erreur : " . $e->getMessage();
+}
+
+
+header('location:index.php');
