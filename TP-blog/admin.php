d478798ae4c4f647fe288c03ee2eaef86478dab7
myrdinaptaliesen <nicog.dev@gmail.com>
2020-06-15 16:47:27 +0200

    Create admin.php

---
+++ TP-blog/admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP-blog</title>
+</head>
+<body>   
+    <h2>Creation de posts</h2>
+
+    
+
+    <form action="form.php" method="post">
+        <input type="text" name="titre" >
+        <textarea name="contenu"  cols="30" rows="10">Entrez le contenu du post</textarea>
+        <input type="submit" value="Créer">
+    </form>
+</body>
+</html>
