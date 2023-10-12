cb5a280560bf630a05e2d3d30b653388710b22bb
myrdinaptaliesen <nicog.dev@gmail.com>
2023-10-12 14:43:03 +0200

    Update index.php

--- Simudes/index.php
+++ Simudes/index.php
@@ -1,39 +1,93 @@
 <?php
-    require 'header.php';
+require 'header.php';
 ?>
 
 <?php
-    if (!empty($_POST)) {
-        $result = $_POST['result'];
-    };
+// if (!empty($_POST)) {
+//     $result = $_POST['result'];
+// };
 
-    if (!empty($_GET)){
-        $faces = $_GET['faces'];
-    }else{
-        $faces = 6;
+if ($_POST) {
+    $nbdes = $_POST['nbdes'];
+} else {
+    $nbdes = 0;
+}
+
+if ($_GET) {
+    $faces = $_GET['faces'];
+    for ($i = 0; $i < $nbdes; $i++) {
+        $results[] = rand(1, $faces);
     }
-    
+} else {
+    $faces = 6;
+}
+
+
+
+
 ?>
 <main>
-<h1>Tirage de dé à <?php echo $faces ?> faces</h1>
-<div class="results">
-    
-<div class="circleCenter">
+    <h1>Tirage de dé à <?= $faces ?> faces</h1>
+    <div class="results">
+
+
+        <!----------------------- Avec un dé -------------------------->
+
+        <!-- <div class="circleCenter">
 <?php
 
-if ($_POST) { ?>
+if (!empty($_POST)) { ?>
         <div class='total'>
-            <span><?=  $result ?></span>
+            <span><?=$result ?></span>
         </div>
 <?php
-    };
+};
 ?>
-</div>
+</div> -->
 
-</div>
-<form action="index.php?faces=<?php echo $faces ?>" method="post">
-    <input class="submit btn shadow-1 rounded-1 large primary uppercase" type="submit" value="Lancer le dé">
-</form>
+        <!----------------------- Avec un ou plusieurs dés -------------------------->
+        <?php
+        for ($i = 0; $i < $nbdes; $i++) {
+
+        ?>
+
+            <div class="circleCenter">
+                <?php
+
+                if (!empty($_POST)) { ?>
+                    <div class='total'>
+                        <span><?= $results[$i] ?></span>
+                    </div>
+                <?php
+                };
+                ?>
+            </div>
+
+        <?php
+        };
+        ?>
+
+
+
+
+    </div>
+    <form action="index.php?faces=<?php echo $faces ?>" method="post">
+        <label class="txt-center" for="select">choisissez le nombre de dés</label>
+        <select class="form-control rounded-1" id="select" name="nbdes">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+        </select>
+        <!-- <input type="hidden" name="result" value="<?php echo rand(1, $faces) ?>"> -->
+        <input class="submit btn shadow-1 rounded-1 large primary uppercase" type="submit" value="Lancer le dé">
+    </form>
 
 </main>
 
